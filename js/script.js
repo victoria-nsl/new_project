97868e99c5f0504a81cbc43f99a5a07b559f4d24
victoria-nsl <vbnaish@yandex.ru>
2022-12-04 11:32:07 +0300

    добавляет работу аккордионов

--- js/script.js
+++ js/script.js
@@ -1,35 +1,59 @@
 /*---------------Aккордион--------------*/
-const blockAccordion = document.querySelector(".accordion__list");
+const blockAccordionPlatformInstallation = document.querySelector(
+  ".accordion--platform-installation"
+);
+const itemsAccordionPlatformInstallation =
+  blockAccordionPlatformInstallation.querySelectorAll(".accordion__item");
+const triggersPlatformInstallation =
+  blockAccordionPlatformInstallation.querySelectorAll(".accordion__button");
 
-if (blockAccordion) {
-  const itemsAccordion = blockAccordion.querySelectorAll(".accordion__item");
-  const triggers = blockAccordion.querySelectorAll(".accordion__button");
+const blockAccordionFaq = document.querySelector(".accordion__list--faq");
+const itemsAccordionFaq =
+  blockAccordionFaq.querySelectorAll(".accordion__item");
+const triggersFaq = blockAccordionFaq.querySelectorAll(".accordion__button");
 
-  const hideContent = (item) => {
-    item.classList.remove("accordion__item--active");
-    item.classList.add("accordion__item--closed");
-  };
+const hideContent = (item) => {
+  item.classList.remove("accordion__item--active");
+  item.classList.add("accordion__item--closed");
+};
 
-  const showContent = (item) => {
-    item.classList.add("accordion__item--active");
-    item.classList.remove("accordion__item--closed");
-  };
+const showContent = (item) => {
+  item.classList.add("accordion__item--active");
+  item.classList.remove("accordion__item--closed");
+};
 
-  triggers.forEach((trigger, index) => {
-    trigger.addEventListener("click", () => {
-      const itemAccordionCurrent = itemsAccordion[index];
+triggersFaq.forEach((trigger, index) => {
+  trigger.addEventListener("click", () => {
+    const itemAccordionCurrent = itemsAccordionFaq[index];
 
-      if (itemAccordionCurrent.classList.contains("accordion__item--active")) {
-        hideContent(itemAccordionCurrent);
-        return;
+    if (itemAccordionCurrent.classList.contains("accordion__item--active")) {
+      hideContent(itemAccordionCurrent);
+      return;
+    }
+
+    itemsAccordionFaq.forEach((itemAccordion) => {
+      if (itemAccordion.classList.contains("accordion__item--active")) {
+        hideContent(itemAccordion);
       }
-
-      itemsAccordion.forEach((itemAccordion) => {
-        if (itemAccordion.classList.contains("accordion__item--active")) {
-          hideContent(itemAccordion);
-        }
-      });
-      showContent(itemAccordionCurrent);
     });
+    showContent(itemAccordionCurrent);
   });
-}
+});
+
+triggersPlatformInstallation.forEach((trigger, index) => {
+  trigger.addEventListener("click", () => {
+    const itemAccordionCurrent = itemsAccordionPlatformInstallation[index];
+
+    if (itemAccordionCurrent.classList.contains("accordion__item--active")) {
+      hideContent(itemAccordionCurrent);
+      return;
+    }
+
+    itemsAccordionPlatformInstallation.forEach((itemAccordion) => {
+      if (itemAccordion.classList.contains("accordion__item--active")) {
+        hideContent(itemAccordion);
+      }
+    });
+    showContent(itemAccordionCurrent);
+  });
+});
